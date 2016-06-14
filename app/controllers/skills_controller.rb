22119cc274b391ca9077d6fd701d8cbe58808a86
naridas <gregsparta@gmail.com>
2016-06-14 17:19:51 +0100

    Created tests

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,6 @@
 class SkillsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @user = current_user
     @skills = @user.skills.all
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -18,6 +18,15 @@ def sign_up(username = "myUsername")
 	click_button("Sign up")
 end
 
+def create_skill
+	visit '/skills'
+	expect(current_path).to have_content('/skills')
+	fill_in("Skill", with: 'Cooking')
+	select 'Advanced', from: 'Skilllevel'
+	fill_in("Description", with: 'BBQ')
+	click_button "Create Skill"
+end
+
 def remove_uploaded_file
 	FileUtils.rm_rf(Rails.root + "public/system")
 end
