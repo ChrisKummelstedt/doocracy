5ba61e9c887883b063bff8eb255b67830281a836
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-06-14 12:18:44 +0200

    added resources to routes config file

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613154606) do
+ActiveRecord::Schema.define(version: 20160614090612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "skilllists", force: :cascade do |t|
+    t.string   "skill"
+    t.string   "skilllevel"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "skill"
+    t.string   "skilllevel"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
