98dbbbade7070c0c2a8311996cdf18959bff9989
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-06-14 12:47:02 +0200

    ajax and jquery working

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -12,13 +12,17 @@ class SkillsController < ApplicationController
   end
 
   def destroy
-    skill = Skill.find(params[:id])
-    skill.destroy
-    redirect_to skills_path
+    @skill = Skill.find(params[:id])
+    @skill.destroy
+
+    respond_to do |format|
+      format.html { redirect_to skills_path }
+      format.js { }
+    end
   end
 
   private
-    def skill_params
-      params.require(:skill).permit(:skill, :skilllevel, :description)
-    end
+  def skill_params
+    params.require(:skill).permit(:skill, :skilllevel, :description)
+  end
 end
