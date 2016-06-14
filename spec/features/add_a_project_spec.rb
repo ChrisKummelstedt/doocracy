f5e8101c47a622bf8dd9c382de5bfae5287aa819
RichardWatkins1 <richardwatkins1@gmail.com>
2016-06-14 12:02:12 +0100

    2-create-a-project complete

--- spec/features/add_a_project_spec.rb
+++ spec/features/add_a_project_spec.rb
@@ -8,11 +8,11 @@ feature "Add a project" do
     click_link "Add a Project"
     expect(current_path).to eq(new_project_path)
     expect(page).to have_content("Let the journey begin")
-    fill_in("title", with: "awesome project title")
-    fill_in("description", with: "Looks like an awesome")
-    fill_in("total budget", with: 1000)
+    fill_in("Title", with: "awesome project title")
+    fill_in("Description", with: "Looks like an awesome")
+    fill_in("Total budget", with: 1000)
     attach_file('Image', "spec/files/images/project-puzzle.jpg")
-    click_button "submit"
+    click_button "Create Project"
     expect(current_path).to eq(projects_path)
     expect(page).to have_content("awesome project title")
     expect(page).to have_content("Looks like an awesome")
