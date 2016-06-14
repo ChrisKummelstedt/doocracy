a007184f93764cee0de270e2c5691af7769068bf
RichardWatkins1 <richardwatkins1@gmail.com>
2016-06-14 14:49:43 +0100

    2 adding a project complete image size set as thumb all tests passing

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
   validates :user_id, :title, :description, presence: true
   belongs_to :user
 
-  has_attached_file :image, :style => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
