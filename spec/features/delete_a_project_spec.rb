5d7240d38ee24a07b293b1bcd4665fee22cd932e
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-06-15 12:34:24 +0200

    delete works, tested

---
+++ spec/features/delete_a_project_spec.rb
@@ -0,0 +1,26 @@
+
+feature "Delete a project" do
+  after do
+    remove_uploaded_file
+  end
+
+  scenario "removing a project correctly" do
+    create_project_1
+    click_link "My Projects"
+    click_link "Edit"
+    click_button "Delete"
+    expect(page).should_not have_content("awesome project title")
+    expect(page).to have_content 'Project deleted successfully'
+  end
+
+  # scenario "cant remove a project unless you are owner" do
+  #   create_project_1
+  #   click_link "Logout"
+  #   sign_up_2
+  #   visit "/projects/2/edit"
+  #   click_button "Delete"
+  #   expect(page).to have_content("awesome project title")
+  #   expect(page).to have_content("You can't delete other peoples projects!")
+  # end
+
+end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -18,6 +18,16 @@ def sign_up(username = "myUsername")
 	click_button("Sign up")
 end
 
+def sign_up_2(username = "myUsername2")
+	visit("/")
+	click_link("Sign up")
+	fill_in("User name", with: username)
+	fill_in("Email", with: "test2@example.com")
+	fill_in("Password", with: "testtest")
+	fill_in("Password confirmation", with: "testtest")
+	click_button("Sign up")
+end
+
 def create_skill
 	visit '/skills'
 	expect(current_path).to have_content('/skills')
