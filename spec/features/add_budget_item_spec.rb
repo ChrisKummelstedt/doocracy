005c542f4f671ceab90876d13602cb5a522504e8
RichardWatkins1 <richardwatkins1@gmail.com>
2016-06-15 17:19:57 +0100

    delete budget items and add them tested

--- spec/features/add_budget_item_spec.rb
+++ spec/features/add_budget_item_spec.rb
@@ -6,8 +6,27 @@ feature "Budget item" do
   scenario "adding a budget item" do
     create_project
     create_team
-    click_link "Coding Team"
-    expect(current_path).to eq('/projects/1/teams/1')
+    visit('/budgets')
+    fill_in("Budget item", with: "Hammer")
+    fill_in("Quantity", with: 1)
+    fill_in("Cost per item", with: 3)
+    click_button "Add budget item"
+    expect(page).to have_content("Hammer")
+    expect(page).to have_content(1)
+    expect(page).to have_content(3.0)
+  end
 
+  scenario "delete a budget item" do
+    create_project
+    create_team
+    visit('/budgets')
+    fill_in("Budget item", with: "Hammer")
+    fill_in("Quantity", with: 1)
+    fill_in("Cost per item", with: 3)
+    click_button "Add budget item"
+    click_link("delete budget item")
+    expect(page).not_to have_content("Hammer")
+    expect(page).not_to have_content(1)
+    expect(page).not_to have_content(3.0)
   end
 end
