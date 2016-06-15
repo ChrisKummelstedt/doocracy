18e8a9ab2d6b029372e17b19d43be96400d5a503
RichardWatkins1 <richardwatkins1@gmail.com>
2016-06-15 16:59:19 +0100

    can add items to budget list and delete them

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,10 +1,29 @@
 class BudgetsController < ApplicationController
+
   def index
-    @budget_items = Budget.budget_item.all
+    @budgets = Budget.all
   end
 
   def create
-    Budget.create(budget_params)
-    redirect_to root_path
+    @budget = Budget.create(budget_params)
+    respond_to do |format|
+      format.html { redirect_to budgets_path }
+      format.js { }
+    end
+  end
+
+  def destroy
+    @budget = Budget.find(params[:id])
+    @budget.destroy
+    respond_to do |format|
+    format.html { redirect_to budgets_path }
+    format.js { }
+    end
+  end
+
+  private
+
+  def budget_params
+    params.require(:budget).permit(:title, :budget_item, :quantity, :cost_per_item)
   end
 end
