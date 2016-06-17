4ddeb66c898bc0cc414ef29aa7f61e4493acfd9b
Nick <nm.richards@yahoo.com>
2016-06-17 07:34:13 -0400

    Feature and unit tests added

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,12 +2,14 @@ class BudgetsController < ApplicationController
   before_action :set_project
 
   def index
-    @budgets = Budget.all
+    @team = Team.find(params[:team_id])
+    @budget = @team.budgets.all
   end
 
   def create
     @team = Team.find(params[:team_id])
     @budget = @team.budgets.create(budget_params)
+    @budgets = @team.budgets.all
     respond_to do |format|
       format.html { redirect_to project_team_path(@project, @team) }
       format.js { }
@@ -16,6 +18,7 @@ class BudgetsController < ApplicationController
 
   def destroy
     @team = Team.find(params[:team_id])
+    @budgets = @team.budgets.all
     @budget = @team.budgets.find(params[:id])
     @budget.destroy
     respond_to do |format|
---
+++ spec/models/budgets_spec.rb
@@ -0,0 +1,9 @@
+describe Budget, type: :model do
+  let!(:budget) do
+    Budget.create(budget_item: "pizza", quantity: "3",
+                  cost_per_item: "3")
+  end
+  it "adds total of budget items" do
+    expect(Budget.first.total(Budget.all)).to eq(9)
+  end
+end
