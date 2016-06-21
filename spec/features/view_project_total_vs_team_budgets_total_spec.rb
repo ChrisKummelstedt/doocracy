879c2a660469fcc36ac685157b95a17b1e5d9d85
RichardWatkins1 <richardwatkins1@gmail.com>
2016-06-21 17:25:46 +0100

    test added for editing a team as a non signed up user

--- spec/features/view_project_total_vs_team_budgets_total_spec.rb
+++ spec/features/view_project_total_vs_team_budgets_total_spec.rb
@@ -12,6 +12,5 @@ feature "viewing graph of team budgets vs project budget" do
     expect(page).to have_content("awesome project title")
     expect(page).to have_content("1000")
     page.html.should include("{series.name}: {point.y}<br/>Total: {point.stackTotal}")
-    expect(page).to have_css("img[src*='project-puzzle.jpg']")
   end
 end
