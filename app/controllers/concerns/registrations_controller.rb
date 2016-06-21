082b376eaee0081ebe3057c8dca068b5ad886d3a
naridas <gregsparta@gmail.com>
2016-06-21 11:09:08 +0100

    fixed fb login & rspec and some syntax

--- app/controllers/concerns/registrations_controller.rb
+++ app/controllers/concerns/registrations_controller.rb
@@ -6,4 +6,7 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:email, :user_name, :password, :password_confirmation)
   end
 
+  def account_update_params
+    params.require(:user).permit(:email, :user_name, :password, :password_confirmation, :current_password)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  validates :title, :description, :inventory_id presence: true  
+  validates :title, :description, :inventory_id, presence: true  
   belongs_to :inventory
 
   has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }
