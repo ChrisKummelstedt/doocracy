a864a651a2669a5f765e58ce4cad1540da4a1044
naridas <gregsparta@gmail.com>
2016-06-21 11:37:32 +0100

    some small fixes

--- app/controllers/concerns/registrations_controller.rb
+++ app/controllers/concerns/registrations_controller.rb
@@ -5,8 +5,4 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:email, :user_name, :password, :password_confirmation)
   end
-
-  def account_update_params
-    params.require(:user).permit(:email, :user_name, :password, :password_confirmation, :current_password)
-  end
 end
