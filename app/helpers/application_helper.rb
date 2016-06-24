6cafa8119eaba16d95819da464e4bbd0128cd1fb
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-06-24 10:51:59 +0200

    logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,16 +4,21 @@ module ApplicationHelper
     return image_tag user.avatar.url(:medium),
     id: 'image-preview',
     class: 'img-responsive img-circle profile-image' if user.avatar.exists?
-    image_tag 'default-avatar.jpg', id: 'image-preview',
-    class: 'img-responsive img-circle profile-image'
-  end
+      image_tag 'default-avatar.jpg', id: 'image-preview',
+      class: 'img-responsive img-circle profile-image'
+      end
 
-  def alert_for(flash_type)
-    {
-        :success => 'alert-success',
-        :error => 'alert-danger',
-        :alert => 'alert-warning',
-        :notice => 'alert-info'
-    }[flash_type.to_sym] || flash_type.to_s
-  end
-end
+      def alert_for(flash_type)
+        {
+          :success => 'alert-success',
+          :error => 'alert-danger',
+          :alert => 'alert-warning',
+          :notice => 'alert-info'
+          }[flash_type.to_sym] || flash_type.to_s
+        end
+
+        def link_to_image(image_path, target_link,options={})
+          link_to(image_tag(image_path, :border => "0"), target_link, options)
+        end
+
+      end
